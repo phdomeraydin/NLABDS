ef08d0d955cca9ce569d8246162e217b816dd330
Omer Aydin <omeraydin35@gmail.com>
2026-08-26 19:40:03 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from src import ExperimentConfig, LieSignatureExperimentApp
-
-
-if __name__ == "__main__":
-    config = ExperimentConfig(output_dir="results")
-    app = LieSignatureExperimentApp(config)
-    app.run()
